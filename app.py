b212f460d6e2a37a5918382cacd1a3be5bdfe0f0
Zhicong Zeng <zzeng1@seattleu.edu>
2022-05-28 19:27:15 -0700

    Succeed implement.

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from asyncio.windows_events import NULL
+from email import message
+from webbrowser import get
 from flask import Flask , render_template , json , request
 import os
 
@@ -7,20 +10,41 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def root():
-  return "<p>Hello, World!</p>"
+  return "<p>Hello, User! Please choose your application type!</p>"
 
+DataList = []
 
-@app.route('/common_get',methods = ['GET'])
-def common_get():
-  file_path = os.path.relpath("Data\CommonSettingData.json")
-  with open(file_path) as file:
-    commons = json.load(file)
-  return render_template('common.html', title="page", jsonfile=json.dumps(commons))
-
-
-@app.route('/common_post',methods = ['POST'])
-def common_post():
+@app.route('/ApplicationC',methods = ['POST', 'GET'])
+def ApplicationC():
   if request.method == 'POST':
-    file = request.files['json'] 
-    file_path = os.path.relpath("Data\CommonSettingData.json")
-    file.save(file_path)
+    json_file = request.files['file'].read().decode("utf-8")
+    json_obj = json.loads(json_file)
+    message = "<p>"
+    
+    if(json_obj.get("CommonSettingData")):
+      common_setting_data = json_obj["CommonSettingData"]
+      file_path = os.path.relpath("Data\CommonSettingData.json")
+      with open(file_path, 'w') as outfile:
+        json.dump(common_setting_data, outfile)
+      message += "Succeed change common data"
+        
+    if(json_obj.get("ApplicationC")):
+      application_c = json_obj["ApplicationC"]
+      file_path = os.path.relpath("Data\ApplicationC.json")
+      with open(file_path, 'w') as outfile:
+        json.dump(application_c, outfile)
+      message += "Succeed change ApplicationC data"
+    
+    message += "</p>"
+    return message
+  
+  else:#Get method
+    DataList.clear()
+    Common_file_path = os.path.relpath("Data\CommonSettingData.json")
+    with open(Common_file_path) as file:
+      DataList.append(json.load(file))
+    AppC_file_path = os.path.relpath("Data\ApplicationC.json")
+    with open(AppC_file_path) as file:
+      DataList.append(json.load(file))
+    return render_template('ApplicationC.html', title="page", jsonfile=json.dumps(DataList))
+
