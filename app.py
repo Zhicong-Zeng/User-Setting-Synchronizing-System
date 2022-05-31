337e5ad5cceb92c37a8fdc4e11be876d83383714
Zhicong Zeng <zzeng1@seattleu.edu>
2022-05-30 23:16:09 -0700

    Combine the logic together.

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-#from asyncio.windows_events import NULL
-from email import message
-from webbrowser import get
 from flask import Flask , render_template , json , request
 import os
 import json
@@ -38,7 +35,7 @@ def ApplicationA():
     message += "Successfully updated ApplicationA data"
     
     message += "</p>"
-    readAppAIntoAppToCommonMapping()
+    readAppIntoAppToCommonMapping("ApplicationA")
     return message
   
   else:#Get method
@@ -47,7 +44,6 @@ def ApplicationA():
     with open(AppA_file_path) as file:
       DataList.append(json.load(file))
     return render_template('ApplicationA.html', title="page", jsonfile=json.dumps(DataList))
-
 
 
 @app.route('/ApplicationB',methods = ['POST', 'GET'])
@@ -71,7 +67,7 @@ def ApplicationB():
     message += "Successfully updated ApplicationB data"
     
     message += "</p>"
-    readAppBIntoAppToCommonMapping()
+    readAppIntoAppToCommonMapping("ApplicationB")
     return message
   
   else:#Get method
@@ -80,8 +76,6 @@ def ApplicationB():
     with open(AppB_file_path) as file:
       DataList.append(json.load(file))
     return render_template('ApplicationB.html', title="page", jsonfile=json.dumps(DataList))
-
-
 
 
 @app.route('/ApplicationC',methods = ['POST', 'GET'])
@@ -104,7 +98,7 @@ def ApplicationC():
     message += "Successfully updated ApplicationC data"
     
     message += "</p>"
-    readAppCIntoAppToCommonMapping()
+    readAppIntoAppToCommonMapping("ApplicationC")
     return message
   
   else:#Get method
@@ -115,52 +109,59 @@ def ApplicationC():
     return render_template('ApplicationC.html', title="page", jsonfile=json.dumps(DataList))
 
 
-
-
-def readAppAIntoAppToCommonMapping():
-    appTocommon_dict = readAppTocommonMappingIntoDict();
-    metadata_dict = readJsonIntoMetadataMapping();
-    file_path = os.path.relpath("Data/ApplicationA.json")
+def readAppIntoAppToCommonMapping(ApplcationName):
+    appTocommon_dict = readAppTocommonMappingIntoDict()
+    metadata_dict = readJsonIntoMetadataMapping()
+    file_path = os.path.relpath("Data/" + ApplcationName + ".json")
     with open(file_path) as file:
-        appA_dict = json.loads(file.read())
-    for key in appA_dict:
-        if (key in appTocommon_dict["ApplicationA"]):
-            value = appA_dict[key]
-            result = metadata_dict[appTocommon_dict["ApplicationA"][key]]
-            updateApps("Save_Copy/ApplicationB.json", result[1], value)
-            updateApps("Save_Copy/ApplicationC.json", result[2], value)
-            updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationA"][key], value)
+        app_dict = json.loads(file.read())
+    for key in app_dict:
+        if (key in appTocommon_dict[ApplcationName]):
+            value = app_dict[key]
+            result = metadata_dict[appTocommon_dict[ApplcationName][key]]
+            if(ApplcationName == "ApplicationA"):
+              updateApps("Save_Copy/ApplicationB.json", result[1], value)
+              updateApps("Save_Copy/ApplicationC.json", result[2], value)
+              updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict[ApplcationName][key], value)
+            elif(ApplcationName == "ApplicationB"):
+              updateApps("Save_Copy/ApplicationA.json", result[0], value)
+              updateApps("Save_Copy/ApplicationC.json", result[2], value)
+              updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict[ApplcationName][key], value)
+            elif(ApplcationName == "ApplicationC"):
+              updateApps("Save_Copy/ApplicationA.json", result[0], value)
+              updateApps("Save_Copy/ApplicationB.json", result[1], value)
+              updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict[ApplcationName][key], value)
     print("Successfully changed the data")
 
-def readAppBIntoAppToCommonMapping():
-    appTocommon_dict = readAppTocommonMappingIntoDict();
-    metadata_dict = readJsonIntoMetadataMapping();
-    file_path = os.path.relpath("Data/ApplicationB.json")
-    with open(file_path) as file:
-        appB_dict = json.loads(file.read())
-    for key in appB_dict:
-        if (key in appTocommon_dict["ApplicationB"]):
-            value = appB_dict[key]
-            result = metadata_dict[appTocommon_dict["ApplicationB"][key]]
-            updateApps("Save_Copy/ApplicationA.json", result[0], value)
-            updateApps("Save_Copy/ApplicationC.json", result[2], value)
-            updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationB"][key], value)
-    print("Successfully changed the data")
+# def readAppBIntoAppToCommonMapping():
+#     appTocommon_dict = readAppTocommonMappingIntoDict();
+#     metadata_dict = readJsonIntoMetadataMapping();
+#     file_path = os.path.relpath("Data/ApplicationB.json")
+#     with open(file_path) as file:
+#         appB_dict = json.loads(file.read())
+#     for key in appB_dict:
+#         if (key in appTocommon_dict["ApplicationB"]):
+#             value = appB_dict[key]
+#             result = metadata_dict[appTocommon_dict["ApplicationB"][key]]
+#             updateApps("Save_Copy/ApplicationA.json", result[0], value)
+#             updateApps("Save_Copy/ApplicationC.json", result[2], value)
+#             updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationB"][key], value)
+#     print("Successfully changed the data")
 
-def readAppCIntoAppToCommonMapping():
-    appTocommon_dict = readAppTocommonMappingIntoDict();
-    metadata_dict = readJsonIntoMetadataMapping();
-    file_path = os.path.relpath("Data/ApplicationC.json")
-    with open(file_path) as file:
-        appC_dict = json.loads(file.read())
-    for key in appC_dict:
-        if (key in appTocommon_dict["ApplicationC"]):
-            value = appC_dict[key]
-            result = metadata_dict[appTocommon_dict["ApplicationC"][key]]
-            updateApps("Save_Copy/ApplicationA.json", result[0], value)
-            updateApps("Save_Copy/ApplicationB.json", result[1], value)
-            updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationC"][key], value)
-    print("Successfully changed the data")
+# def readAppCIntoAppToCommonMapping():
+#     appTocommon_dict = readAppTocommonMappingIntoDict();
+#     metadata_dict = readJsonIntoMetadataMapping();
+#     file_path = os.path.relpath("Data/ApplicationC.json")
+#     with open(file_path) as file:
+#         appC_dict = json.loads(file.read())
+#     for key in appC_dict:
+#         if (key in appTocommon_dict["ApplicationC"]):
+#             value = appC_dict[key]
+#             result = metadata_dict[appTocommon_dict["ApplicationC"][key]]
+#             updateApps("Save_Copy/ApplicationA.json", result[0], value)
+#             updateApps("Save_Copy/ApplicationB.json", result[1], value)
+#             updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationC"][key], value)
+#     print("Successfully changed the data")
 
 def readJsonIntoMetadataMapping():
     file_path = os.path.relpath("Data/CommonMetadataMapping.json")
@@ -168,11 +169,13 @@ def readJsonIntoMetadataMapping():
         metadata_dict = json.loads(file.read())
     return metadata_dict
     
+
 def readAppTocommonMappingIntoDict():
     file_path = os.path.relpath("Data/AppToCommonMapping.json")
     with open(file_path) as file:
         appTocommon_dict = json.loads(file.read())
     return appTocommon_dict
+
 
 def updateApps(appPath, sourceKey, sourceValue):
     file_path = os.path.relpath(appPath)
@@ -183,18 +186,4 @@ def updateApps(appPath, sourceKey, sourceValue):
             application_dict[key] = sourceValue
     with open(appPath, 'w') as outfile:
         json.dump(application_dict, outfile)
-
-@app.route('/common_get',methods = ['GET'])
-def common_get():
-  file_path = os.path.relpath("Data/CommonSettingData.json")
-  with open(file_path) as file:
-    commons = json.load(file)
-  return render_template('common.html', title="page", jsonfile=json.dumps(commons))
-
-@app.route('/common_post',methods = ['POST'])
-def common_post():
-  if request.method == 'POST':
-    file = request.files['json'] 
-    file_path = os.path.relpath("Data/CommonSettingData.json")
-    file.save(file_path)
 
