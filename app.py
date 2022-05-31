3b2db5b451453e845f002945721558aa9889d4bc
amalor <62664476+amalor@users.noreply.github.com>
2022-05-30 19:17:59 -0700

    Merge branch 'main' of https://github.com/Zhicong-Zeng/User-Setting-Synchronizing-System

--- app.py
+++ app.py
@@ -148,3 +148,57 @@ def updateApps(appPath, sourceKey, sourceValue):
     with open(appPath, 'w') as outfile:
         json.dump(application_dict, outfile)
     
+
+
+
+@app.route('/ApplicationA',methods = ['POST', 'GET'])
+def ApplicationA():
+  if request.method == 'POST':
+    json_file = request.files['file'].read().decode("utf-8")
+    json_obj = json.loads(json_file)
+    message = "<p>"
+    
+    if(json_obj.get("CommonSettingData")):
+      common_setting_data = json_obj["CommonSettingData"]
+      file_path = os.path.relpath("Save_Copy/CommonSettingData.json")
+      with open(file_path, 'w') as outfile:
+        json.dump(common_setting_data, outfile)
+      message += "Succeed change common data"
+        
+
+    file_path = os.path.relpath("Save_Copy/ApplicationA.json")
+    with open(file_path, 'w') as outfile:
+      json.dump(json_obj, outfile)
+    message += "Successfully updated ApplicationA data"
+    
+    message += "</p>"
+    readAppAIntoAppToCommonMapping()
+    return message
+  
+  else:#Get method
+    DataList.clear()
+    AppA_file_path = os.path.relpath("Save_Copy/ApplicationA.json")
+    with open(AppA_file_path) as file:
+      DataList.append(json.load(file))
+    return render_template('ApplicationA.html', title="page", jsonfile=json.dumps(DataList))
+
+
+def readAppAIntoAppToCommonMapping():
+    appTocommon_dict = readAppTocommonMappingIntoDict();
+    metadata_dict = readJsonIntoMetadataMapping();
+    file_path = os.path.relpath("Data/ApplicationA.json")
+    with open(file_path) as file:
+        appA_dict = json.loads(file.read())
+    for key in appA_dict:
+        if (key in appTocommon_dict["ApplicationA"]):
+            value = appA_dict[key]
+            result = metadata_dict[appTocommon_dict["ApplicationA"][key]]
+            updateApps("Save_Copy/ApplicationB.json", result[1], value)
+            updateApps("Save_Copy/ApplicationC.json", result[2], value)
+            updateApps("Save_Copy/CommonSettingData.json", appTocommon_dict["ApplicationA"][key], value)
+    print("Successfully changed the data")
+
+
+
+if __name__ == '__main__':
+    app.run()
